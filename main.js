558dfceb9a17fea3cd00006f80c16e15bdab45a4
Ruben <rvalseca@labster.com>
2023-08-14 16:29:00 +0200

    store data in url hash

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
-const { createApp, ref, computed } = Vue;
+const { createApp, ref, computed, watchEffect } = Vue;
 
 createApp({
   setup: () => {
-    const participants = ref([
-      { name: 'Angélica', payed: 13 },
-      { name: 'Bubango', payed: 52 },
-      { name: 'Jota', payed: 114 },
-      { name: 'Irene', payed: 4.5 },
-      { name: 'Rubén', payed: 165 },
-    ]);
+    const urlParticipants = decodeURI(location.hash).slice(1);
+    const participants = ref(urlParticipants ? JSON.parse(urlParticipants) : []);
+    watchEffect(() => {
+      const hash = participants.value.length
+        ? JSON.stringify(participants.value)
+        : '';
+      location.hash = hash;
+    });
+
+    function addParticipant() {
+      const name = prompt('Participant name');
+      if (participants.value.some(participant => participant.name === name)) {
+        alert('Participant already exists');
+        return;
+      }
+      participants.value.push({ name, payed: 0 });
+    }
+    function removeParticipant(participant) {
+      participants.value.splice(participants.value.indexOf(participant), 1);
+    }
+
 
     const total = computed(() => {
       return participants.value.reduce((acc, participant) =>  acc + participant.payed, 0);
@@ -47,6 +61,8 @@ createApp({
 
     return {
       participants,
+      addParticipant,
+      removeParticipant,
       total,
       payments,
     };
